76f53bc9a51c2c48d12e85f5fc630cb292196ba3
SuJin <1127sujink@naver.com>
2022-12-04 20:18:25 +0900

    [FE] feat: footer 완성
    로고 넣고, github링크 넣고, 팀원, 저작권 추가

--- client/kiri/src/App.js
+++ client/kiri/src/App.js
@@ -1,5 +1,15 @@
+import { Footer } from "./components/Footer";
+import './App.css';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <section>
+
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
