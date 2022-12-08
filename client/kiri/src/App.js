55dd5aac72e3b79922ab3c9e242ca6b7bf2d74e6
jwo0o0 <jwo0o0358@gmail.com>
2022-12-08 17:02:12 +0900

    [FE] style: Globalstyle 변경

--- client/kiri/src/App.js
+++ client/kiri/src/App.js
@@ -2,20 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles/Globalstyle';
 import MainPage from './pages/MainPage';
 import NoMatchPage from './pages/NoMatchPage';
-import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="*" element={<NoMatchPage />} />
-          </Routes>
-        </ThemeProvider>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="*" element={<NoMatchPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
