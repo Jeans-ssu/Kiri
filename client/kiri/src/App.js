82cc2e0f7ff3c8a88b3674130fe36ce253528daf
jwo0o0 <jwo0o0358@gmail.com>
2022-12-08 20:38:15 +0900

    [FE] fix: 폴더 세팅

--- client/kiri/src/App.js
+++ client/kiri/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from 'styles/Globalstyle';
 import MainPage from 'pages/MainPage';
