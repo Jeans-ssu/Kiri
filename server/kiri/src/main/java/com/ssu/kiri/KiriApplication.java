01cf850475da587826c3943932d441bcdba8ad20
lovee9523 <noeun1102@gmail,com>
2022-12-28 23:48:15 +0900

    initial setting

---
+++ server/kiri/src/main/java/com/ssu/kiri/KiriApplication.java
@@ -0,0 +1,13 @@
+package com.ssu.kiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiriApplication.class, args);
+	}
+
+}
---
+++ server/kiri/src/test/java/com/ssu/kiri/KiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssu.kiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
