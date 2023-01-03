40bd840613688418ac559cf5c20793f9c7d3580f
SuJin <1127sujink@naver.com>
2023-01-03 21:25:51 +0900

    ✨ [FE] feat: Searh Page 완성

---
+++ client/kiri/src/pages/Event/EventSearchPage.js
@@ -0,0 +1,132 @@
+import styled from 'styled-components';
+import PageContainer from 'containers/PageContainer';
+import { BsEyeFill } from 'react-icons/bs';
+
+const pos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const EventSearchPage = () => {
+  return (
+    <PageContainer header footer>
+      <EventKeywordPageContainer>
+        <h1>&apos;해커톤&apos; 검색 결과</h1>
+        <h4 className="titlesearch">(제목에서 검색한 결과입니다)</h4>
+        <EventListMain>
+          {pos.map((el) => (
+            <EventContainer key={el}>
+              <EventList>
+                <h4>2023 제목</h4>
+                <p className="host">주최</p>
+                <div className="flex">
+                  <p className="dday">D-nn</p>
+                  <div className="flex">
+                    <BsEyeFill size="12" className="eyeicon" />
+                    <p className="watch">조회수</p>
+                  </div>
+                </div>
+              </EventList>
+              <EventImg>
+                <img
+                  className="poster"
+                  alt="poster"
+                  src={`${process.env.PUBLIC_URL}/poster.jpg`}
+                ></img>
+              </EventImg>
+            </EventContainer>
+          ))}
+        </EventListMain>
+      </EventKeywordPageContainer>
+    </PageContainer>
+  );
+};
+
+const EventKeywordPageContainer = styled.div`
+  h1 {
+    color: black;
+    padding-left: 30px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+
+  .titlesearch {
+    text-align: center;
+    font-weight: 500;
+    color: #898989;
+  }
+
+  .flex {
+    display: flex;
+  }
+`;
+
+const EventListMain = styled.main`
+  display: flex;
+  justify-content: row;
+  flex-wrap: wrap;
+  margin-left: 122px;
+  margin-top: 30px;
+`;
+
+const EventContainer = styled.div`
+  width: 360px;
+  height: 210px;
+  display: flex;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  padding-left: 30px;
+  padding: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+const EventList = styled.div`
+  justify-content: row;
+  width: 200px;
+
+  h4 {
+    margin-top: 0px;
+    margin-bottom: 0px;
+    cursor: pointer;
+  }
+
+  .host {
+    font-size: 14px;
+    color: #636363;
+  }
+
+  .dday {
+    margin-top: 0;
+    font-size: 12px;
+    color: #59b89d;
+  }
+
+  .watch {
+    margin-top: 0;
+    font-size: 12px;
+    color: #737373;
+    margin-left: 3px;
+  }
+
+  .eyeicon {
+    margin-left: 10px;
+    margin-top: 2.8px;
+    color: #737373;
+  }
+`;
+
+const EventImg = styled.div`
+  width: 150px;
+  img {
+    justify-content: column;
+    width: 100%;
+    top: 50%;
+    left: 50%;
+    position: absoulte;
+    padding: 0 10px;
+    cursor: pointer;
+  }
+`;
+
+export default EventSearchPage;
