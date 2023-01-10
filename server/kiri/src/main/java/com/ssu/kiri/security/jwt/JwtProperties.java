44dda3f5693317c1027a8b4461d8eec73adb1996
lovee9523 <noeun1102@gmail,com>
2023-01-10 23:01:12 +0900

    :lock: [BE] jwt 유효기간 변경 (300분 -> 3일)

--- server/kiri/src/main/java/com/ssu/kiri/security/jwt/JwtProperties.java
+++ server/kiri/src/main/java/com/ssu/kiri/security/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.ssu.kiri.security.jwt;
 
 public interface JwtProperties {
     String SECRET = "SECRET";
-    int EXPIRATION_TIME = 18000000; // 300분 (1/1000초)
+    int EXPIRATION_TIME = 259200000; // 3일 (1/1000초) , 최대 2,147,483,647
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
