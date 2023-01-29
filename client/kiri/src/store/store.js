ffb5a4526e1791bf502ebae588b2cacbce24ff2a
jwo0o0 <jwo0o0358@gmail.com>
2023-01-29 23:38:57 +0900

    :sparkles: [FE] feat: redux-persist 도입

--- client/kiri/src/store/store.js
+++ client/kiri/src/store/store.js
@@ -1,13 +1,28 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../store/modules/counterSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import authReducer from './modules/authSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
 
 export const rootReducer = combineReducers({
   counter: counterReducer,
+  auth: authReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
