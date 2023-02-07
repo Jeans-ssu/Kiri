b612a83947098887f25cbfdc0862bd56fd360101
lovee9523 <noeun1102@gmail,com>
2023-02-07 22:00:05 +0900

    :white_check_mark: [BE] 좋아요 누르기, 취소 Controller, Service 테스트 추가

--- server/kiri/src/test/java/com/ssu/kiri/scrap/ScrapServiceTest.java
+++ server/kiri/src/test/java/com/ssu/kiri/scrap/ScrapServiceTest.java
@@ -1,61 +1,34 @@
 package com.ssu.kiri.scrap;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.ssu.kiri.infra.WithAccount;
 import com.ssu.kiri.member.MemberRepository;
 import com.ssu.kiri.post.Post;
 import com.ssu.kiri.post.PostRepository;
 import com.ssu.kiri.post.PostService;
 import com.ssu.kiri.scrap.dto.ScrapReqAdd;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+
+@SpringBootTest
 @Transactional
-class ScrapControllerTest {
-
-    @Autowired
-    private WebApplicationContext ctx;
-    private ObjectMapper objectMapper = new ObjectMapper();
-    private MockMvc mockMvc;
+class ScrapServiceTest {
     @Autowired PostRepository postRepository;
     @Autowired MemberRepository memberRepository;
     @Autowired ScrapRepository scrapRepository;
     @Autowired PostService postService;
     @Autowired ScrapService scrapService;
-
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 한글 깨짐 처리
-//                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-    }
 
     @AfterEach
     void afterEach() {
@@ -65,7 +38,7 @@ class ScrapControllerTest {
     }
 
     @WithAccount("creamyyyy")
-    @DisplayName("좋아요 누르기")
+    @DisplayName("게시글 좋아요 누르기 테스트")
     @Test
     public void addScrapTest() throws Exception {
         //given
@@ -94,23 +67,18 @@ class ScrapControllerTest {
         request.setEndScrapTime(LocalDateTime.parse("2022-11-25 12:10:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
 
-        //when & then
-        this.mockMvc.perform(
-                        MockMvcRequestBuilders // MockMvcRequestBuilders 를 안쓰면 get 함수를 인식 못함
-                                .post("/extra/{id}", savedPostId) // 넣어준 컨트롤러의 Http Method 와 URL 을 지정
-                                .accept(MediaType.APPLICATION_JSON) // accept encoding 타입을 지정
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.registerModule(new JavaTimeModule()).writeValueAsString(request))
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
+        //when
+        boolean isScrap = scrapService.addScrap(savedPostId, request);
+
+        //then
+        assertThat(isScrap).isEqualTo(true);
 
     }
 
     @WithAccount("creamyyyy")
     @DisplayName("게시글 좋아요 취소 테스트")
     @Test
-    public void deleteScrap() throws Exception {
+    public void deleteScrapTest() throws Exception {
         //given
 
         // creamyyy 가 post 등록.
@@ -136,18 +104,17 @@ class ScrapControllerTest {
         request.setStartScrapTime(LocalDateTime.parse("2022-11-25 12:10:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         request.setEndScrapTime(LocalDateTime.parse("2022-11-25 12:10:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
+        // 좋아요 완료
         boolean isScrap = scrapService.addScrap(savedPostId, request);
 
-        //when & then
-        this.mockMvc.perform(
-                        MockMvcRequestBuilders // MockMvcRequestBuilders 를 안쓰면 get 함수를 인식 못함
-                                .post("/calenders/extra/{id}", savedPostId) // 넣어준 컨트롤러의 Http Method 와 URL 을 지정
-                                .accept(MediaType.APPLICATION_JSON) // accept encoding 타입을 지정
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
+        //when
+        scrapService.deleteScrap(savedPostId);
+
+        //then
+
 
     }
+
 
 
 }
