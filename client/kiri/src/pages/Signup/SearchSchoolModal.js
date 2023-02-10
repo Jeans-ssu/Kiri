9bc2e0e06ed89119bec613cfbab18c97b5bf562b
jwo0o0 <jwo0o0358@gmail.com>
2023-02-10 20:56:40 +0900

    :sparkles: [FE] feat: 학교 검색 모달에 검색창 추가

--- client/kiri/src/pages/Signup/SearchSchoolModal.js
+++ client/kiri/src/pages/Signup/SearchSchoolModal.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { IoMdClose } from 'react-icons/io';
+import { FiSearch } from 'react-icons/fi';
 
 const ModalContainer = styled.div``;
 
@@ -19,12 +20,16 @@ const ModalView = styled.div`
   width: 450px;
   height: 350px;
   background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const ModalHeader = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
+  width: 100%;
   svg {
     position: absolute;
     top: 20px;
@@ -35,8 +40,30 @@ const ModalHeader = styled.div`
   }
   div {
     margin-top: 20px;
-    font-weight: 600;
+    font-weight: 400;
+    font-size: 14px;
   }
+`;
+
+const SchoolSearchbarContainer = styled.div`
+  position: relative;
+  svg {
+    position: absolute;
+    right: 8px;
+    top: 18px;
+    color: ${({ theme }) => theme.colors.mainColor};
+  }
+`;
+
+const SchoolSearchbar = styled.input`
+  box-sizing: border-box;
+  width: 350px;
+  height: 30px;
+  margin-top: 12px;
+  border: 1px solid ${({ theme }) => theme.colors.lightgray};
+  border-radius: 5px;
+  padding: 0 5px;
+  outline: none;
 `;
 
 const SearchSchoolModal = ({ isOpen, setIsOpen }) => {
@@ -50,9 +77,13 @@ const SearchSchoolModal = ({ isOpen, setIsOpen }) => {
         <ModalBackdrop onClick={openModalHandler}>
           <ModalView onClick={(e) => e.stopPropagation()}>
             <ModalHeader>
-              <div>학교 검색</div>
+              <div>재학/소속 중인 대학교 이름을 검색해주세요.</div>
               <IoMdClose onClick={openModalHandler} />
             </ModalHeader>
+            <SchoolSearchbarContainer>
+              <SchoolSearchbar />
+              <FiSearch />
+            </SchoolSearchbarContainer>
           </ModalView>
         </ModalBackdrop>
       ) : null}
