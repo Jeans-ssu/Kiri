759bd20fe7fc629c537e7a073ade898753e3f9c6
jwo0o0 <jwo0o0358@gmail.com>
2023-02-14 22:36:33 +0900

    :sparkles: [FE] feat: 미로그인 유저 마이페이지 접근 차단
    미로그인 상태에서 마이페이지 접근시 메인페이지로 이동함

--- client/kiri/src/pages/Mypage/Mypage.js
+++ client/kiri/src/pages/Mypage/Mypage.js
@@ -1,6 +1,10 @@
 import PageContainer from 'containers/PageContainer';
 import styled from 'styled-components';
 import MypageInputs from './MypageInputs';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { selectIsLogin } from 'store/modules/userSlice';
+import { useEffect } from 'react';
 
 const MypageContainer = styled.div`
   display: flex;
@@ -16,6 +20,14 @@ const MypageHeader = styled.div`
 `;
 
 const Mypage = () => {
+  const isLogin = useSelector(selectIsLogin);
+  const navigate = useNavigate();
+
+  //미로그인 상태에서 마이페이지 접근시 메인페이지로 이동
+  useEffect(() => {
+    if (!isLogin) navigate('/');
+  }, [isLogin]);
+
   return (
     <PageContainer header footer={false} margin_bottom={false}>
       <MypageContainer>
