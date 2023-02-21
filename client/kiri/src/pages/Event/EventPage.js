7d86968e1eb901d0ed57526d61d23a6ba2926bc1
SuJin <1127sujink@naver.com>
2023-02-21 15:28:23 +0900

    ✨ [FE] feat: 학교 검색 수정

--- client/kiri/src/pages/Event/EventPage.js
+++ client/kiri/src/pages/Event/EventPage.js
@@ -5,10 +5,20 @@ import { FaSearch } from 'react-icons/fa';
 import { EventTag } from './EventTag';
 import { Link } from 'react-router-dom';
 import EventContent from './EventContent';
+import SearchUnivModal from 'pages/Signup/SearchUnivModal';
 
 const EventPage = () => {
   const [click, setClick] = useState(false);
   const [currentNav, setCurrentNav] = useState(-1);
+  const [searchuniv, setSearchUniv] = useState('');
+
+  //학교 검색 모달
+  const [showUnivModal, setShowUnivModal] = useState(false);
+
+  const setUserUniv = (univName) => {
+    console.log(univName);
+    setSearchUniv(univName);
+  };
 
   const selectFilterHandler = () => {
     setClick(true);
@@ -22,6 +32,14 @@ const EventPage = () => {
     setClick(!click);
     fieldClick[idx] = !fieldClick[idx];
     console.log(field[idx], fieldClick, currentNav);
+  };
+
+  const handleClickSearchUnivBtn = () => {
+    setShowUnivModal(true);
+  };
+
+  const removeUniv = () => {
+    setSearchUniv('');
   };
 
   const filter = ['학교', '지역'];
@@ -58,8 +76,21 @@ const EventPage = () => {
                 type="text"
                 id="text"
                 placeholder="원하는 학교를 검색해보세요"
+                value={searchuniv}
               ></SearchInput>
+              <OpenSearchModalBtn>
+                {searchuniv === '' ? (
+                  <Findtxt onClick={handleClickSearchUnivBtn}>찾아보기</Findtxt>
+                ) : (
+                  <Removetxt onClick={removeUniv}>선택안함</Removetxt>
+                )}
+              </OpenSearchModalBtn>
             </Searchdiv>
+            <SearchUnivModal
+              isOpen={showUnivModal}
+              setIsOpen={setShowUnivModal}
+              setUserUniv={setUserUniv}
+            />
           </SchoolSearchContainer>
         </TopBox>
         <CheckboxDiv>
@@ -152,7 +183,7 @@ const SearchInput = styled.input`
   border-radius: 3px;
   padding: 7.8px 9.1px 7.8px 32px;
   width: 240px;
-
+  color: black;
   margin-left: 5px;
   display: flex;
   align-items: center;
@@ -160,7 +191,12 @@ const SearchInput = styled.input`
   &:focus {
     outline: none;
   }
-  font-size: 12px;
+  font-size: 14px;
+  font-weight: 700;
+  ::placeholder {
+    font-weight: 500;
+    font-size: 12px;
+  }
 `;
 
 const CheckboxDiv = styled.div`
@@ -180,6 +216,30 @@ const CheckboxDiv = styled.div`
 const FilterLi = styled.li`
   list-style: none;
   cursor: pointer;
+`;
+
+const OpenSearchModalBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 80px;
+  margin-left: -5px;
+  background-color: transparent;
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Findtxt = styled.div`
+  color: ${({ theme }) => theme.colors.mainColor};
+  font-weight: 600;
+`;
+
+const Removetxt = styled.div`
+  color: ${({ theme }) => theme.colors.lightgray};
+  text-decoration: underline;
+  font-weight: 500;
 `;
 
 export default EventPage;
