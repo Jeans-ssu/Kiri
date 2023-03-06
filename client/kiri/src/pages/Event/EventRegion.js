cbd9670f53a3199f2b785c0b4792fcc1f11b023f
SuJin <1127sujink@naver.com>
2023-03-06 22:43:00 +0900

    💄 [FE] style: region page에도 정렬 추가

--- client/kiri/src/pages/Event/EventRegion.js
+++ client/kiri/src/pages/Event/EventRegion.js
@@ -9,6 +9,7 @@ const EventRegion = () => {
   const [click, setClick] = useState(false);
   const [currentNav, setCurrentNav] = useState(-1);
   const [interest, setInterest] = useState('IT'); // 지역 select
+  const [order, setOrder] = useState('최신순');
 
   const selectFilterHandler = () => {
     setClick(true);
@@ -26,6 +27,10 @@ const EventRegion = () => {
 
   const handleChangeInterest = (e) => {
     setInterest(e.target.value);
+  };
+
+  const handleChangeOrder = (e) => {
+    setOrder(e.target.value);
   };
 
   const filter = ['학교', '지역'];
@@ -88,11 +93,37 @@ const EventRegion = () => {
           ))}
         </CheckboxDiv>
         <Bar />
+        <EventOrderBox>
+          <OrderInput onChange={handleChangeOrder} value={order}>
+            <option value="최신순">최신순</option>
+            <option value="좋아요순">좋아요순</option>
+          </OrderInput>
+        </EventOrderBox>
         <EventContent />
       </EventFieldPageContainer>
     </PageContainer>
   );
 };
+
+const EventOrderBox = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+const OrderInput = styled.select`
+  width: 85px;
+  height: 30px;
+  border: none;
+  border-radius: 3px;
+  padding-left: 5px;
+  margin-left: auto;
+  margin-right: 30px;
+  font-weight: 600;
+  font-size: 14px;
+  &:focus {
+    outline: none;
+  }
+`;
 
 const EventFieldPageContainer = styled.div`
   /* color: ${({ theme }) => theme.colors.mainColor}; */
