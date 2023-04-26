04ca34c0e2fcafe830180670b3e55ef07d9f64cb
lovee9523 <noeun1102@gmail,com>
2023-04-26 20:01:31 +0900

    :lock: [BE] cors 허용 출처 수정

--- server/kiri/src/main/java/com/ssu/kiri/config/WebMvcConfig.java
+++ server/kiri/src/main/java/com/ssu/kiri/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*") // 허용할 HTTP METHOD
-                .allowedOriginPatterns("*") // 허용할 출처
+                .allowedOriginPatterns("http://localhost:3000", "http://kiriuniv.com/") // 허용할 출처
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization"); // 반환하고싶은 헤더값
