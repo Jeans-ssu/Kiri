62b4097d3d4ab658cbb8df607167015a3f12d949
lovee9523 <noeun1102@gmail,com>
2023-05-13 16:48:30 +0900

    :card_file_box: [BE] Entity "tag" 추가

---
+++ server/kiri/src/main/java/com/ssu/kiri/tag/Tag.java
@@ -0,0 +1,21 @@
+package com.ssu.kiri.tag;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String tagname;
+
+}
